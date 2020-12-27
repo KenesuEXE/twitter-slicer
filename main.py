e43b34b47351d4cddcd7ec03c89100bba378abcf
KenesuEXE <76158324+KenesuEXE@users.noreply.github.com>
2020-12-27 11:38:29 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+import twitter-slicer.py
+
+print("""
+The TWITTER SLICER
+Slice your lengthy text into tweetable chunks!
+
+made by: @KenesuEXE, u/KnnthKnnth
+""")
+
+text = input("Paste your text here: (or of you want a sample text, type 'example')\n")
+text = example if text == "example"
+
+mode = input("""
+Select Mode
+1 - Raw Slice
+2 - Clean Slice \n
+""")
+
+counter = input("Add counter? (Y/N) \n")
 
