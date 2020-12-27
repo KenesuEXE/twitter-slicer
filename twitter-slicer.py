88c6957db04bb99ce2eb0b1cd119c99b4d4825e8
KenesuEXE <76158324+KenesuEXE@users.noreply.github.com>
2020-12-27 11:31:59 +0800

    update twitter-slicer.py

--- twitter-slicer.py
+++ twitter-slicer.py
@@ -1,8 +1,3 @@
-print("""
-TWITTER SLICER
-made by: @KenesuEXE u/KnnthKnnth
-""")
-
 #Raw Slice
 #Slices the text into strictly 280-character chunks.
 def raw_slice(text):
@@ -17,7 +12,7 @@ def clean_slice(text):
   cs = []
   i = 0
   while i+280 < len(text): #Gets the first 280 chars.
-    for z in range(i+280, i-1, -1): #Starting from the 280th, go backwards  and find  the nearest whitespace.
+    for z in range(i+280, i-1, -1): #Starting from the 280th, go backwards  and find the nearest whitespace.
       if text[z].isspace():
         cs.append(text[i:z]) #Append from start up to the last word.
         i = z + 1 #The new starting point will be where we have just cutted it.
@@ -47,29 +42,4 @@ def clean_counted_slice(text):
   ccs.append(text[i:len(text)] + " ({}/{})".format(len(text)//272+1, len(text)//272+1))
   return ccs
 
-
-#INPUT
-#Paste your text here! 
 example = "What the fuck did you just fucking say about me, you little bitch? I'll have you know I graduated top of my class in the Navy Seals, and I've been involved in numerous secret raids on Al-Quaeda, and I have over 300 confirmed kills. I am trained in gorilla warfare and I'm the top sniper in the entire US armed forces. You are nothing to me but just another target. I will wipe you the fuck out with precision the likes of which has never been seen before on this Earth, mark my fucking words. You think you can get away with saying that shit to me over the Internet? Think again, fucker. As we speak I am contacting my secret network of spies across the USA and your IP is being traced right now so you better prepare for the storm, maggot. The storm that wipes out the pathetic little thing you call your life. You're fucking dead, kid. I can be anywhere, anytime, and I can kill you in over seven hundred ways, and that's just with my bare hands. Not only am I extensively trained in unarmed combat, but I have access to the entire arsenal of the United States Marine Corps and I will use it to its full extent to wipe your miserable ass off the face of the continent, you little shit. If only you could have known what unholy retribution your little \"clever\" comment was about to bring down upon you, maybe you would have held your fucking tongue. But you couldn't, you didn't, and now you're paying the price, you goddamn idiot. I will shit fury all over you and you will drown in it. You're fucking dead, kiddo."
-
-#OUTPUT
-rs = raw_slice(example)
-cs = clean_slice(example)
-rcs = raw_counted_slice(example)
-ccs = clean_counted_slice(example)
-
-print("\n Raw Slice")
-for e in rs:
-  print ('\n', e)
-  
-print("\n Clean Slice")
-for e in cs:
-  print ('\n', e)
-
-print("\n Raw Counted Slice")
-for e in rcs:
-  print ('\n', e)
-
-print("\n Clean Counted Slice")
-for e in ccs:
-  print ('\n', e)
