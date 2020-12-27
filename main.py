04f14923a598962451e7f63e9a971ea44076cd36
KenesuEXE <76158324+KenesuEXE@users.noreply.github.com>
2020-12-27 18:13:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,34 @@
-import twitter-slicer.py
+import twitterslicer
 
 print("""
 The TWITTER SLICER
 Slice your lengthy text into tweetable chunks!
-
-made by: @KenesuEXE, u/KnnthKnnth
+made by: KenesuEXE — u/KnnthKnnth\n
 """)
 
-text = input("Paste your text here: (or of you want a sample text, type 'example')\n")
-text = example if text == "example"
+text = input("""
+Paste your text here:
+(If you want a sample text, input 'example') \n
+""")
+if text == "example":
+  text = twitterslicer.example
 
 mode = input("""
-Select Mode
+Choose a slicing mode:
 1 - Raw Slice
-2 - Clean Slice \n
+2 - Clean Slice
+3 - Raw Counted Slice
+4 - Clean Counted Slice \n
 """)
 
-counter = input("Add counter? (Y/N) \n")
+if mode == "1":
+  tweets = twitterslicer.raw_slice(text)
+elif mode == "2":
+  tweets = twitterslicer.clean_slice(text)
+elif mode == "3":
+  tweets = twitterslicer.raw_counted_slice(text)
+elif mode == "4":
+  tweets = twitterslicer.clean_counted_slice(text)
 
+for chunks in tweets:
+  print("\n", chunks)
