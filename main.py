24fd9086ea8000f95d992619b7478acfab9a61e5
KenesuEXE <76158324+KenesuEXE@users.noreply.github.com>
2021-01-01 18:15:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,51 @@
 import twitterslicer
 
-print("""
-The TWITTER SLICER
-by KenesuEXE \n
-""")
 
-text = input ("""
-Paste your text here:
-(If you want to use a sample text, input 'sample')
-\n""")
-
-if text == 'sample':
-  text = twitterslicer.sample
-
-mode = input ("""
+def main():
+    mode_query = input("""
 Pick a slicing mode:
 1 - By limit
 2 - By space
 3 - By punctuation
-\n""")
+    \n""")
 
-count = input("""
+    counter_query = input("""
 Add counter?
 1 - Yes
 2 - No
-\n""")
+    \n""")
 
-if count.lower() == "1":
-  counter = True
-elif count.lower() == "2":
-  counter = False
-else:
-  counter = True
-  print ("\nCounter input error, set to default: True")
-  
-if mode == "1":
-  tweets = twitterslicer.by_limit(text, counter)
-elif mode == "2":
-  tweets = twitterslicer.by_space(text, counter)
-elif mode == "3":
-  tweets = twitterslicer.by_punctuation(text, counter)
-else:
-  print("\nMode input error, set to default: Punctuation")
-  tweets = twitterslicer.by_punctuation(text, counter)
-  
-for tweet in tweets:
-  print ("\n", tweet)
+    if counter_query == "1":
+        counter = True
+    elif counter_query == "2":
+        counter = False
+    else:  # Wrong Input
+        counter = True
+        print("\nCounter input error, set to default: True")
+
+    if mode_query == "1":
+        tweets = twitterslicer.by_limit(text, counter)
+    elif mode_query == "2":
+        tweets = twitterslicer.by_space(text, counter)
+    elif mode_query == "3":
+        tweets = twitterslicer.by_punctuation(text, counter)
+    else:  # Wrong  input
+        tweets = twitterslicer.by_punctuation(text, counter)
+        print("\nMode input error, set to default: Punctuation")
+
+    print("\nHere are your tweets:")
+    for tweet in tweets:  # Prints all elements in list 'tweets'.
+        print("\n", tweet)
+
+if __name__ == "__main__":    
+    print("TWITTER SLICER v2 by KenesuEXE\n")
+
+    while True:  # Loops script so it can be used multiple times
+
+        with open('texthere.txt') as f:  # Open and read texthere.txt
+            text = f.read()
+            if text == "":  # Check if texthere.txt is blank
+                print("Paste your text first in texthere.txt")
+                break
+        
+        main()
