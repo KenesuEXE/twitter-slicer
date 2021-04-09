6da72dc642f0dcafeb16ab754cc35a9cf45929d9
KenesuEXE <knnthcristobal03@gmail.com>
2021-04-09 22:30:27 +0800

    Added indicated slicing and code improvements

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
 import argparse
-from twitterslicer import *
+import twitterslicer as ts
 
 
 def main():
 
     # Command line flags
-    parser = argparse.ArgumentParser(description="A Twitter thread maker that slices a text into tweetable chunks.")
+    parser = argparse.ArgumentParser(description="a Twitter thread maker that slices a text into tweetable chunks.")
     slice_mode = parser.add_mutually_exclusive_group()
-    parser.add_argument("file_name", help='insert text file')
+    parser.add_argument("file_name", help='insert text file name')
     slice_mode.add_argument("-l", "--limit", action="store_true", help='slice to fit Tweet char limit')
-    slice_mode.add_argument("-s", "--space", action="store_true", help='slice in whitespace before limit')
+    slice_mode.add_argument("-s", "--space", action="store_true", help='slice in whitespace before limit, set to default')
     slice_mode.add_argument("-p", "--punct", action="store_true", help='slice in punctuation mark before limit')
     parser.add_argument("-u", "--uncount", action="store_true", help='remove counter')
     parser.add_argument("-t", "--tweet", action="store_true", help='tweet thread directly to Twitter (API keys required)')
+    parser.add_argument("-sep", "--separator", default="\\", help='set separation indicator, default set to \\')
     args = parser.parse_args()
 
-    # Open and read text file
+    # Retrieve text
     with open(args.file_name) as text_file:
         text = text_file.read()
+    if text == "":
+        raise Exception("Text file should contain text")
 
-    # Set counter mode
-    if args.uncount:
-        counter = False
-    else:
-        counter = True
+    # Set separator and slice on indicated chars
+    sep = args.separator
 
-    slice_tweets = Slice(text, counter)
+    text_list = ts.Slice.indicated(text, sep)
 
-    # Set slice mode
+    # Set counter and slicing mode
+    counter = False if args.uncount else True
+
+    Slice = ts.Slice(text_list, counter)
+
     if args.limit:
-        tweets = slice_tweets.limit()
+        tweets = Slice.limit()
     elif args.space:
-        tweets = slice_tweets.space()
+        tweets = Slice.space()
     elif args.punct:
-        tweets = slice_tweets.punct()
+        tweets = Slice.punct()
     else:
-        tweets = slice_tweets.space()
+        tweets = Slice.space()
 
-    # Output
+    # Print output
     print("Here are your tweets:")
     for tweet in tweets:
         print("\n", tweet)
 
-    # Directly tweet the thread
+    # Create Twitter thread
     if args.tweet:
-        tweet_thread(tweets)
+        ts.tweet_thread(tweets)
 
-    print("\nAll done! Twitter-Slicer shutting down...")
+    print("\nAll done! Twitter Slicer shutting down...")
 
 if __name__ == "__main__":
     main()
--- twitterslicer.py
+++ twitterslicer.py
@@ -1,85 +1,141 @@
-from keys import *
-import textwrap
-
-
 class Slice:
-    def __init__(self, text, counter):
-        self.text = text
+    def __init__(self, text_list, counter):
+        self.text_list = text_list
         self.counter = counter
         self.tweets = []
 
     def limit(self):
+        """Slice text to fit tweet character limit"""
 
-        # By Limit with counter
+        # By limit with counter
         if self.counter:
-            for index in range(0, len(self.text), 272):
-                self.tweets.append(self.text[index:index+272] +
-                                   " ({}/{})".format(index//272+1, len(self.text)//272+1))
+            count = 0
+            for text in self.text_list:
+                while text:
+                    count += 1
+                    self.tweets.append(f"{text[:272]} ({count}/")
+                    text = text[272:]
+            self.tweets = [f"{text}{count})" for text in self.tweets]
 
         # By Limit without counter
         else:
-            for index in range(0, len(self.text), 280):
-                self.tweets.append(self.text[index:index+280])
+            for text in self.text_list:
+                while text:
+                    self.tweets.append(text[:280])
+                    text = text[280:]
 
         return self.tweets
 
     def space(self):
+        """Slice text in whitespace before tweet limit"""
 
         # By Space with counter
         if self.counter:
-            tweets_temp = textwrap.wrap(self.text, width=272)
-            for tweet in tweets_temp:
-                self.tweets.append(tweet + " ({}/{})".format
-                                   (tweets_temp.index(tweet)+1, len(tweets_temp)))
+            count = 0
+            for text in self.text_list:
+                while len(text) > 272:
+                    for index in range(271,-1,-1):
+                        if text[index].isspace():
+                            count += 1
+                            self.tweets.append(f"{text[:index]} ({count}/")
+                            text = text[index+1:]
+                            break
+                    else:
+                        count += 1
+                        self.tweets.append(f"{text[:272]} ({count}/")
+                        text = text[272:]
+                else:
+                    count += 1
+                    self.tweets.append(f"{text} ({count}/")
+            self.tweets = [f"{text}{count})" for text in self.tweets]
 
         # By Space without counter
         else:
-            self.tweets = textwrap.wrap(self.text, width=280)
+            for text in self.text_list:
+                while len(text) > 280:
+                    for index in range(279,-1,-1):
+                        if text[index].isspace():
+                            self.tweets.append(text[:index])
+                            text = text[index+1:]
+                            break
+                    else:
+                        self.tweets.append(text[:280])
+                        text = text[280:]
+                else:
+                    self.tweets.append(text)
 
         return self.tweets
 
     def punct(self):
-        punct_marks = '.!?'
-        index = 0
+        """Slice text in punctuation marks before tweet limit"""
+        punct_marks = ".?!"
 
         # By Punctuation with counter
         if self.counter:
-            tweet_temp = []
-            count = 1
-
-            while index+272 < len(self.text):
-                for punct_search in range(index+272, index-1, -1):
-                    if self.text[punct_search] in punct_marks:
-                        tweet_temp.append(self.text[index:punct_search+1] +
-                                          " ({}/".format(count))
-                        index = punct_search+2
+            count = 0
+            for text in self.text_list:
+                while len(text) > 272:
+                    for index in range(271,-1,-1):
+                        if text[index] in punct_marks:
+                            count += 1
+                            self.tweets.append(f"{text[:index+1]} ({count}/")
+                            text = text[index+2:]
+                            break
+                    else:
                         count += 1
-                        break
-            tweet_temp.append(self.text[index:len(self.text)] +
-                              " ({}/".format(count))
-
-            for tweet in tweet_temp:
-                self.tweets.append(tweet + "{})".format(count))
+                        self.tweets.append(f"{text[:272]} ({count}/")
+                        text = text[272:]
+                else:
+                    count += 1
+                    self.tweets.append(f"{text} ({count}/")
+            self.tweets = [f"{text}{count})" for text in self.tweets]
 
         # By Punctuation without counter
         else:
-            while index+280 < len(self.text):
-                for punct_search in range(index+280, index-1, -1):
-                    if self.text[punct_search] in punct_marks:
-                        self.tweets.append(self.text[index:punct_search+1])
-                        index = punct_search+2
-                        break
-            self.tweets.append(self.text[index:len(self.text)])
+            for text in self.text_list:
+                while len(text) > 280:
+                    for index in range(279,-1,-1):
+                        if text[index] in punct_marks:
+                            self.tweets.append(text[:index+1])
+                            text = text[index+2:]
+                            break
+                    else:
+                        self.tweets.append(text[:280])
+                        text = text[280:]
+                else:
+                    self.tweets.append(text)
 
         return self.tweets
 
+    @staticmethod
+    def indicated(text, sep):
+        """Slice text in indicated separators"""
+        text_list = []
+
+        while text:
+
+            for index in range(len(text)):
+                if text[index:index+len(sep)] == sep:
+                    text_list.append(text[:index])
+                    text = text[index+len(sep):]
+                    break
+            else:
+                text_list.append(text)
+                text = None
+    
+        text_list = list(filter(None, text_list))
+    
+        return text_list
+
 
 def tweet_thread(tweets):
+    """Tweet thread to Twitter"""
+    import keys
     import tweepy
 
     print("\nConnecting to Twitter...")
-    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    auth = tweepy.OAuthHandler(keys.CONSUMER_KEY, keys.CONSUMER_SECRET)
+    auth.set_access_token(keys.ACCESS_KEY, keys.ACCESS_SECRET)
     api = tweepy.API(auth)
 
     # Tweet the first tweet
